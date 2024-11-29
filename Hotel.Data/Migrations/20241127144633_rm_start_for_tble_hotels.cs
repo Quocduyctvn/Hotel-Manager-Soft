803c1abc460ef5ceffe0b7d1c964a43521246488
Quocduyctvn <quocduyctvn@gmail.com>
2024-11-29 13:52:37 +0700

    init

---
+++ Hotel.Data/Migrations/20241127144633_rm_start_for_tble_hotels.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class rm_start_for_tble_hotels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StarRating",
+                table: "AppHotel");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Desc",
+                table: "AppRoomCates",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "AppUser",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$10$/si.vLvf6GuFq.e.OOtpTuVYJxIVtd0lBPsl5UHwvggJPssn9hzL.");
+
+            migrationBuilder.UpdateData(
+                table: "AppUser",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Password",
+                value: "$2a$10$/si.vLvf6GuFq.e.OOtpTuVYJxIVtd0lBPsl5UHwvggJPssn9hzL.");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Desc",
+                table: "AppRoomCates",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StarRating",
+                table: "AppHotel",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AppUser",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$10$BuOf5dkS3bU249LX9AwHFObrA79PNUrwqjcHfUkhUp3cWw/gwWQQa");
+
+            migrationBuilder.UpdateData(
+                table: "AppUser",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Password",
+                value: "$2a$10$BuOf5dkS3bU249LX9AwHFObrA79PNUrwqjcHfUkhUp3cWw/gwWQQa");
+        }
+    }
+}
---
+++ Hotel.Data/Migrations/20241128121741_ud_fk_tble_per.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ud_fk_tble_per : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppRolePermissions_AppPermissions_IdPermission",
+                table: "AppRolePermissions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AppRolePermissions_IdPermission",
+                table: "AppRolePermissions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppPermissions",
+                table: "AppPermissions");
+
+            migrationBuilder.AddColumn<int>(
+                name: "IdGroup",
+                table: "AppRolePermissions",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppPermissions",
+                table: "AppPermissions",
+                columns: new[] { "PerCode", "IdGroup" });
+
+            migrationBuilder.UpdateData(
+                table: "AppUser",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$10$WYPH5P7cYMFkqjTeQuLBSuuIiF7E0zWoqgzoZgRr8a89H4nZGo70m");
+
+            migrationBuilder.UpdateData(
+                table: "AppUser",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Password",
+                value: "$2a$10$WYPH5P7cYMFkqjTeQuLBSuuIiF7E0zWoqgzoZgRr8a89H4nZGo70m");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppRolePermissions_IdPermission_IdGroup",
+                table: "AppRolePermissions",
+                columns: new[] { "IdPermission", "IdGroup" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppRolePermissions_AppPermissions_IdPermission_IdGroup",
+                table: "AppRolePermissions",
+                columns: new[] { "IdPermission", "IdGroup" },
+                principalTable: "AppPermissions",
+                principalColumns: new[] { "PerCode", "IdGroup" },
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppRolePermissions_AppPermissions_IdPermission_IdGroup",
+                table: "AppRolePermissions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AppRolePermissions_IdPermission_IdGroup",
+                table: "AppRolePermissions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppPermissions",
+                table: "AppPermissions");
+
+            migrationBuilder.DropColumn(
+                name: "IdGroup",
+                table: "AppRolePermissions");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppPermissions",
+                table: "AppPermissions",
+                column: "PerCode");
+
+            migrationBuilder.UpdateData(
+                table: "AppUser",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$10$/si.vLvf6GuFq.e.OOtpTuVYJxIVtd0lBPsl5UHwvggJPssn9hzL.");
+
+            migrationBuilder.UpdateData(
+                table: "AppUser",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Password",
+                value: "$2a$10$/si.vLvf6GuFq.e.OOtpTuVYJxIVtd0lBPsl5UHwvggJPssn9hzL.");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppRolePermissions_IdPermission",
+                table: "AppRolePermissions",
+                column: "IdPermission");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppRolePermissions_AppPermissions_IdPermission",
+                table: "AppRolePermissions",
+                column: "IdPermission",
+                principalTable: "AppPermissions",
+                principalColumn: "PerCode",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
